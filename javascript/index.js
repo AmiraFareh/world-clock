bc4851e5e62eb8323291d26ef78d4a45839ecfb6
AmiraFareh <amirafareh137@gmail.com>
2023-03-31 19:03:14 +1300

    Added link to all cities

--- javascript/index.js
+++ javascript/index.js
@@ -47,7 +47,7 @@ function updateCity(event) {
             <div class="date">${cityDate}</div>
           </div>
           <div class="time">${cityTime}</div>
-        </div>`;
+        </div> <a href = "/">All cities</a>`;
 }
 
 let citiesSelectElement = document.querySelector("#city");
